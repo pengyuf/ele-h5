d7df9ecce2c5bdc6f40031d3cdbcdb88ee49d852
best <2907647445@qq.com>
2025-06-08 11:00:39 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
+  ad
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
