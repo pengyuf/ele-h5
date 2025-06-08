e8d16610508a7f0567eb3017302717b7808c07b9
best <2907647445@qq.com>
2025-06-08 11:21:09 +0800

    feat: 使用commitlint规范commit提交

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  ad
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
