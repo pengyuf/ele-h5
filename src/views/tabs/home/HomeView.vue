97284b32742820cab088adb0ed9c8f72fe8fc3ae
best <2907647445@qq.com>
2025-06-10 18:41:46 +0800

    feat: 实现useAsync

--- src/views/tabs/home/HomeView.vue
+++ src/views/tabs/home/HomeView.vue
@@ -7,10 +7,14 @@
       ></SearchView>
     </Transition>
     <TheTop :recomments="recomments" @searchClick="toggleSearchView" />
+    {{ pending }}
+    {{ data }}
   </div>
 </template>
 
 <script setup lang="ts">
+import { fetchHomePageData } from '../../../api/home'
+import { useAsync } from '../../../use/useAsync'
 import { useToggle } from '../../../use/useToggle'
 import SearchView from '../../search/SearchView.vue'
 import TheTop from './components/TheTop.vue'
@@ -21,6 +25,8 @@ const recomments = [
   { value: 1, label: '牛腩' },
   { value: 2, label: '色拉' },
 ]
+
+const { pending, data } = useAsync(fetchHomePageData, {})
 </script>
 
 <style setup scoped>
